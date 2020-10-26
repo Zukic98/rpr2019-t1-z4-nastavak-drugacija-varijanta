c83aa01e93b99143f1153476e8e7ebb4b849844b
mzukic5 <mzukic5@etf.unsa.ba>
2020-10-26 19:34:36 +0100

    Doraditi klase i otkloniti probleme

--- src/ba/unsa/etf/rpr/Artikl.java
+++ src/ba/unsa/etf/rpr/Artikl.java
@@ -1,4 +1,8 @@
 package ba.unsa.etf.rpr;
 
 public class Artikl {
+    String imeArtikla = "";
+    int cijenaArtikla = 0,kodArtikla = 0;
+    Artikl(String imeArtikla,int cijenaArtikla,int kodArtikla)
+    {this.imeArtikla =new String(imeArtikla);this.cijenaArtikla = cijenaArtikla;this.kodArtikla = kodArtikla;}
 }
